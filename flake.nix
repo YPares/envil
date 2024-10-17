2354f325373544086509d29a30334641318a58ea
Yves Parès <yves@lili.ai>
2024-10-17 11:38:10 +0200

    Added substituters to flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "A tool to forge custom, isolated & mergeable environments";
   
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
